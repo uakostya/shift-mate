ef08370a29eec1d82a7189d35837b6c156038db1
Kostiantyn Pikhalo <Kostiantyn.Pikhalo-EXT@ldc.com>
2026-01-24 14:33:17 +0200

    Remove the SSR.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,17 +6,12 @@ import {
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import {
-  provideClientHydration,
-  withEventReplay,
-} from '@angular/platform-browser';
 import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideClientHydration(withEventReplay()),
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000',
